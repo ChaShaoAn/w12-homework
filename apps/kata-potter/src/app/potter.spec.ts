fa4704f6901c924379bd45d5231157ffd21e0b2d
ChaShaoAn <kevinchao840713@gmail.com>
2022-05-13 14:35:09 +0800

    add test 2 different books

--- apps/kata-potter/src/app/potter.spec.ts
+++ apps/kata-potter/src/app/potter.spec.ts
@@ -11,20 +11,23 @@ describe('Potter', () => {
   });
 
   test('buy one copy of first book', () => {
-    const potter = new Potter();
     potter.buy(0, 0);
     expect(potter.cost).toBe(0);
   });
 
   test('buy one copy of first book', () => {
-    const potter = new Potter();
     potter.buy(0, 1);
     expect(potter.cost).toBe(8);
   });
 
   test('buy two copy of first book', () => {
-    const potter = new Potter();
     potter.buy(0, 2);
     expect(potter.cost).toBe(8 * 2);
+  });
+  
+  test('buy one copy of first book and one copy of second book', () => {
+    potter.buy(0, 1);
+    potter.buy(1, 1);
+    expect(potter.cost).toBe((8 * 2) * 0.95);
   });
 });
