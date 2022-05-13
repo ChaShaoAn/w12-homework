ab02aebad730c02181a9357b0c2b0afd31700b02
ChaShaoAn <kevinchao840713@gmail.com>
2022-05-13 14:33:05 +0800

    add test 0 and 2 book

--- apps/kata-potter/src/app/potter.spec.ts
+++ apps/kata-potter/src/app/potter.spec.ts
@@ -12,7 +12,19 @@ describe('Potter', () => {
 
   test('buy one copy of first book', () => {
     const potter = new Potter();
+    potter.buy(0, 0);
+    expect(potter.cost).toBe(0);
+  });
+
+  test('buy one copy of first book', () => {
+    const potter = new Potter();
     potter.buy(0, 1);
     expect(potter.cost).toBe(8);
+  });
+
+  test('buy two copy of first book', () => {
+    const potter = new Potter();
+    potter.buy(0, 2);
+    expect(potter.cost).toBe(8 * 2);
   });
 });
